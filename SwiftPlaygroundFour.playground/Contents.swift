1d81466b330b8d2711c2851d9efd692c7a36beaf
Carlton Hui <carltonhui@gmail.com>
2018-03-07 15:39:06 -1000

    Completed Assignment #6

--- SwiftPlaygroundFour.playground/Contents.swift
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -6,21 +6,53 @@ var str = "Hello, playground"
 
 //for number in 1...5
 //{
-    //print(number)
+//    print(number)
+//}
+//
+//var sum = 0
+//for number in 1...5
+//{
+//sum += number
+//    print(sum)
+//}
+//
+//var i = 1
+//while i <= 10
+//{
+    //We want to print i
+//print(i)
+    //We want to add +1 to i for each iteration through the loop
+//    i = i + 1
 //}
 
-var sum = 0
-for number in 1...5
-{
-sum += number
-    print(sum)
-}
+//Coding Challenge #1
+
+//var sum = 0
+//for number in 1...3
+//{
+//    sum += number
+//    print(sum)
+//}
 
 var i = 1
-while i <= 10
+
+while i <= 3
+    
 {
-    //We want to print i
     print(i)
-    //We want to add +1 to i for each iteration through the loop
     i = i + 1
+}
+
+//Coding Challenge #2
+
+//for number in 1...10
+//{
+//    print(number)
+//}
+
+var newSum = 0
+for number in 1...10
+{
+    newSum -= number
+    print(newSum)
 }
